01d50d21e3b7e59aea6e2f33481cd58340bfe915
Dipto <2019-617-833@cse.du.ac.bd>
2022-07-03 23:11:03 +0600

    front page

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hii 123")
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+
+class MainPage(QMainWindow):
+    def __init__(brow):
+        super(MainPage, brow).__init__()
+        brow.browser=QWebEngineView()
+        brow.browser.setUrl(QUrl('http://google.com'))
+       # brow.centralWidget(brow.browser)
+        brow.setCentralWidget(brow.browser)
+        brow.showMaximized()
+
+
+rend = QApplication(sys.argv)
+QApplication.setApplicationName('Py Browser')
+page=MainPage()
+rend.exec_()
