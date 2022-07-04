64496b9040821fddec5ca3088afa0c6d28a96670
Dipto <2019-617-833@cse.du.ac.bd>
2022-07-05 02:37:00 +0600

    adress bar

--- main.py
+++ main.py
@@ -17,24 +17,40 @@ class MainPage(QMainWindow):
         # taskbar
         task_bar = QToolBar()
         brow.addToolBar(task_bar)
+
         # img load
         bck_img = QIcon("icons/arrow-curve-180-left.png")
-        next_img= QIcon("icons/arrow-curve.png")
+        next_img = QIcon("icons/arrow-curve.png")
         reload_img = QIcon("icons/arrow-circle-225-left.png")
 
-        bck = QAction('Back', brow)
+        # back next reload
+        bck = QAction(bck_img, 'Back', brow)
         bck.triggered.connect(brow.browser.back)
         task_bar.addAction(bck)
 
         forwrd = QAction(next_img, 'Forward', brow)
-        bck.triggered.connect(brow.browser.forward)
+        forwrd.triggered.connect(brow.browser.forward)
         task_bar.addAction(forwrd)
 
         rel = QAction(reload_img, 'reload', brow)
-        bck.triggered.connect(brow.browser.reload)
+        rel.triggered.connect(brow.browser.reload)
         task_bar.addAction(rel)
 
+        # address bar
+        brow.add_bar = QLineEdit()
+        # brow.add_bar.setText("http://")
+        brow.add_bar.returnPressed.connect(brow.to_Url)
+        task_bar.addWidget(brow.add_bar)
 
+    # address formatting
+    def to_Url(self):
+        url = self.add_bar.text()
+        if "http://" not in url:
+            url = "http://" + url
+        self.browser.setUrl(QUrl(url))
+
+
+# app run
 rend = QApplication(sys.argv)
 QApplication.setApplicationName('Py Browser')
 page = MainPage()
